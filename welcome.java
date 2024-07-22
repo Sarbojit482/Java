fc21361a5f91ed45a6cf30c32ad6a1ec64f7c428
Sarbojit <145925398+Sarbojit482@users.noreply.github.com>
2024-07-22 09:16:19 +0530

    welcome.java

--- welcome.java
+++ welcome.java
@@ -1,8 +1,3 @@
-/*Name: Sarbojit Ghosh
- * Enroll no.:202303103510302
- * Branch:B.Tech CSE
- * Aim:-Write a Program that displays Welcome to AMTICS.
- */
 public class welcome{
     public static void main(String [] args){
         
